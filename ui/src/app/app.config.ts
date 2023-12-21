d7f7299c163c039cc1c465e15a003dc733cbee8e
AidenPrime <ali.itbxl@gmail.com>
2023-12-21 23:38:14 +0100

    UI: enabling the lazy-loading of the animation package

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
+//import { provideAnimations } from '@angular/platform-browser/animations';
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 import { routes } from './app.routes';
 import { ProductService } from '../services/productservice';
 import { PhotoService } from '../services/photoservice';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), ProductService, PhotoService, provideAnimations()]
+  providers: [provideRouter(routes), ProductService, PhotoService, provideAnimationsAsync()]
 };
