1c2f62cd21d11de706833e2066552c1a7e7d9969
AidenPrime <ali.itbxl@gmail.com>
2023-12-21 23:32:11 +0100

    UI: advancing design adding galleria and panelmenu

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -14,48 +14,195 @@ import { InputGroupModule } from 'primeng/inputgroup'
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
 import { InputTextModule } from 'primeng/inputtext';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { PanelModule } from 'primeng/panel';
+import { SharedModule  } from 'primeng/api';
+import { PhotoService } from '../services/photoservice';
+import { GalleriaModule } from 'primeng/galleria';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, TabMenuModule, TableModule, TabViewModule,
-            TagModule, RatingModule, FormsModule, ButtonModule, InputGroupModule,
-            MenubarModule, InputTextModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, TabMenuModule, TableModule, TabViewModule,
+        TagModule, RatingModule, FormsModule, ButtonModule, InputGroupModule,
+        MenubarModule, InputTextModule, PanelMenuModule, PanelModule, SharedModule,GalleriaModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 
 export class AppComponent implements OnInit {
-  products!: Product[];
-  title = 'Bookshop';
-  items: MenuItem[] | undefined;
-  constructor(private productService: ProductService) {}
-
-  ngOnInit() {
-    this.items = [
+    products!: Product[];
+    title = 'Bookshop';
+    items: MenuItem[] | undefined;
+    items2: MenuItem[] | undefined;
+    images: any[] | undefined;
+    responsiveOptions: any[] = [
         {
-            label: 'Home',
-            icon: 'pi pi-fw pi-globe'
+            breakpoint: '1024px',
+            numVisible: 5
         },
         {
-            label: 'Book1',
-            icon: 'pi pi-fw pi-book'
+            breakpoint: '768px',
+            numVisible: 3
+        },
+        {
+            breakpoint: '560px',
+            numVisible: 1
         }
     ];
-      this.productService.getProductsMini().then((data) => {
-          this.products = data;
-      });
-  }
+    constructor(private productService: ProductService,
+                private photoService:PhotoService) { }
 
-  getSeverity(status: string) {
-      switch (status) {
-          case 'INSTOCK':
-              return 'success';
-          case 'LOWSTOCK':
-              return 'warning';
-          case 'OUTOFSTOCK':
-              return 'danger';
-      }
-      return '';
-  }
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Home',
+                icon: 'pi pi-fw pi-globe'
+            },
+            {
+                label: 'Book1',
+                icon: 'pi pi-fw pi-book'
+            }
+        ];
+        this.items2 = [
+            {
+                label: 'File',
+                icon: 'pi pi-fw pi-file',
+                expanded:true,
+                items: [
+                    {
+                        label: 'New',
+                        icon: 'pi pi-fw pi-plus',
+                        items: [
+                            {
+                                label: 'Bookmark',
+                                icon: 'pi pi-fw pi-bookmark'
+                            },
+                            {
+                                label: 'Video',
+                                icon: 'pi pi-fw pi-video'
+                            }
+                        ]
+                    },
+                    {
+                        label: 'Delete',
+                        icon: 'pi pi-fw pi-trash'
+                    },
+                    { 
+                        separator: true 
+                    },
+                    {
+                        label: 'Export',
+                        icon: 'pi pi-fw pi-external-link'
+                    }
+                ]
+            },
+            {
+                label: 'Edit',
+                icon: 'pi pi-fw pi-pencil',
+                expanded:true,
+                items: [
+                    {
+                        label: 'Left',
+                        icon: 'pi pi-fw pi-align-left'
+                    },
+                    {
+                        label: 'Right',
+                        icon: 'pi pi-fw pi-align-right'
+                    },
+                    {
+                        label: 'Center',
+                        icon: 'pi pi-fw pi-align-center'
+                    },
+                    {
+                        label: 'Justify',
+                        icon: 'pi pi-fw pi-align-justify'
+                    }
+                ]
+            },
+            {
+                label: 'Users',
+                icon: 'pi pi-fw pi-user',
+                items: [
+                    {
+                        label: 'New',
+                        icon: 'pi pi-fw pi-user-plus'
+                    },
+                    {
+                        label: 'Delete',
+                        icon: 'pi pi-fw pi-user-minus'
+                    },
+                    {
+                        label: 'Search',
+                        icon: 'pi pi-fw pi-users',
+                        items: [
+                            {
+                                label: 'Filter',
+                                icon: 'pi pi-fw pi-filter',
+                                items: [
+                                    {
+                                        label: 'Print',
+                                        icon: 'pi pi-fw pi-print'
+                                    }
+                                ]
+                            },
+                            {
+                                icon: 'pi pi-fw pi-bars',
+                                label: 'List'
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                label: 'Events',
+                icon: 'pi pi-fw pi-calendar',
+                items: [
+                    {
+                        label: 'Edit',
+                        icon: 'pi pi-fw pi-pencil',
+                        items: [
+                            {
+                                label: 'Save',
+                                icon: 'pi pi-fw pi-calendar-plus'
+                            },
+                            {
+                                label: 'Delete',
+                                icon: 'pi pi-fw pi-calendar-minus'
+                            }
+                        ]
+                    },
+                    {
+                        label: 'Archieve',
+                        icon: 'pi pi-fw pi-calendar-times',
+                        items: [
+                            {
+                                label: 'Remove',
+                                icon: 'pi pi-fw pi-calendar-minus'
+                            }
+                        ]
+                    }
+                ]
+            }
+        ];
+        this.productService.getProductsMini().then((data) => {
+            this.products = data;
+        });
+        this.photoService.getImages().then(images => {
+            this.images = images;
+        });
+    }
+
+    getSeverity(status: string) {
+        switch (status) {
+            case 'INSTOCK':
+                return 'success';
+            case 'LOWSTOCK':
+                return 'warning';
+            case 'OUTOFSTOCK':
+                return 'danger';
+        }
+        return '';
+    }
 }
--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { ProductService } from '../services/productservice';
+import { PhotoService } from '../services/photoservice';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), ProductService]
+  providers: [provideRouter(routes), ProductService, PhotoService, provideAnimations()]
 };
