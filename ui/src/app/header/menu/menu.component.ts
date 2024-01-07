72d5a05f7cdc422c7846cc75828d2270217c8425
AidenPrime <ali.itbxl@gmail.com>
2024-01-07 20:54:28 +0100

    Update menu.component.ts

--- ui/src/app/header/menu/menu.component.ts
+++ ui/src/app/header/menu/menu.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
 import { Router } from '@angular/router';
+import { CategoryResponseDto } from '../../dto/category/category-response-dto';
+import { CategoryService } from '../../../services/category.service';
 @Component({
   selector: 'app-menu',
   standalone: true,
@@ -11,20 +13,34 @@ import { Router } from '@angular/router';
 })
 export class MenuComponent implements OnInit {
   items: MenuItem[] | undefined;
-
-  constructor(private router: Router){}
+  categories: CategoryResponseDto[] | undefined;
+  constructor(private router: Router, private categoryService: CategoryService) { }
   ngOnInit() {
-    this.items = [
-      {
-        label: 'Home',
-        icon: 'pi pi-fw pi-globe',
-        command: () => this.router.navigate([''])
-      },
-      {
-        label: 'Books',
-        icon: 'pi pi-fw pi-book',
-        command: () => this.router.navigate(['/books'])
+    this.categoryService.getAll().subscribe({
+      next: (r) => {
+        if (r && r.listDto) {
+          this.categories = r.listDto;
+          this.items = [
+            {
+              label: 'Home',
+              icon: 'pi pi-fw pi-globe',
+              command: () => this.router.navigate([''])
+            },
+            {
+              label: 'All Books',
+              icon: 'pi pi-fw pi-book',
+              command: () => this.router.navigate(['/books'])
+            }
+          ];
+          for (let category of this.categories) {
+            this.items?.push({
+              label: category.title,
+              icone: 'pi pi-fw pi-book',
+              command: () => this.router.navigate([`/books/category/${category.id}`])
+            });
+          }
+        }
       }
-    ];
+    });
   }
 }
