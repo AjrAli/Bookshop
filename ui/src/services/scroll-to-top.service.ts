ddbbde945364aa35d97cc5f1d60f20b23b01e904
AidenPrime <ali.itbxl@gmail.com>
2024-01-14 17:59:49 +0100

    Fix scroll on top by types div

--- ui/src/services/scroll-to-top.service.ts
+++ ui/src/services/scroll-to-top.service.ts
@@ -7,11 +7,20 @@ export class ScrollToTopService {
     constructor(router: Router) {
         router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe({
             next: (r) => {
-                //window.scrollTo(0, 0);
-                const targetElement = document.querySelector('#targetRootOutlet');
-                if (targetElement) {
-                    targetElement.scrollIntoView({ behavior: 'smooth' });
+                if (r instanceof NavigationEnd) {
+                    if (!r.url.includes("search")) {
+                        const targetElement = document.querySelector('#targetRootOutlet');
+                        if (targetElement) {
+                            targetElement.scrollIntoView({ behavior: 'smooth' });
+                        }
+                    } else {
+                        const targetSearch = document.querySelector('#targetSearch');
+                        if (targetSearch) {
+                            targetSearch.scrollIntoView({ behavior: 'smooth' });
+                        }
+                    }
                 }
+                //window.scrollTo(0, 0);
             }
         });
     }
