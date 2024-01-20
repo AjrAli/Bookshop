76cb9b8f8616fb4be45f1d0875d8a1be61552ab1
AidenPrime <ali.itbxl@gmail.com>
2024-01-20 09:36:30 +0100

    Update search.component.ts

--- ui/src/app/components/search/search.component.ts
+++ ui/src/app/components/search/search.component.ts
@@ -29,7 +29,7 @@ export class SearchComponent implements OnInit {
     private searchService: SearchService,
     private searchStateService: SearchStateService,
     private toastService: ToastService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.initializeSearchStateService();
